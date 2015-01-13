61b07f67a710587e112e3888408459f4c04b761b
morgan.skinner <morgan.skinner@oakbrookfinance.com>
2015-01-13 10:05:46 +0000

    A few comments

--- FileStorageSpike.Services/AzureFileStore.cs
+++ FileStorageSpike.Services/AzureFileStore.cs
@@ -20,6 +20,11 @@ namespace FileStorageSpike.Services
             _container.CreateIfNotExists();
         }
 
+        /// <summary>
+        /// Get a file given its filename
+        /// </summary>
+        /// <param name="filename">The name of the file</param>
+        /// <returns>A stream containing the file</returns>
         public Stream GetFile(string filename)
         {
             var blockBlob = _container.GetBlockBlobReference(filename);
@@ -29,12 +34,21 @@ namespace FileStorageSpike.Services
             return stream;
         }
 
+        /// <summary>
+        /// Store a file
+        /// </summary>
+        /// <param name="filename">The name of the file</param>
+        /// <param name="fileContent">The content of that file</param>
         public void StoreFile(string filename, Stream fileContent)
         {
             var blockBlob = _container.GetBlockBlobReference(filename);
             blockBlob.UploadFromStream(fileContent);
         }
 
+        /// <summary>
+        /// Return a collection of filenames from this file store
+        /// </summary>
+        /// <returns>A collection of all filenames</returns>
         public IEnumerable<string> GetFilenames()
         {
             List<string> filenames = new List<string>();
